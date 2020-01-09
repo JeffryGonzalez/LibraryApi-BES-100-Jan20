da1651ca6ce98a4b50376a08bb9872b42df1522b
Jeffry Gonzalez <jeff@hypertheory.com>
2020-01-09 15:12:41 +0000

    For Craig

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -17,6 +17,42 @@ namespace LibraryApi.Controllers
             Context = context;
         }
 
+        [HttpPost("/books")]
+        public async Task<IActionResult> AddABook()
+        {
+            // Validate the thingy
+            // Not? -> Return a 400 status (Bad Request)
+            // Is Valid?
+            //   Add it to the database
+            //   Return a 201 (Created) status code.
+            //   Add a location header to the response that has the URL for the new baby resource.
+            //   And, if you are nice, send them a copy of the new resource as well.
+            return BadRequest();
+        }
+
+        // GET /books/{id}
+        [HttpGet("/books/{id:int}")]
+        public async Task<IActionResult> GetABook(int id)
+        {
+            var result = await Context.Books
+                .Select(b => new GetBookResponseDocument
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Author = b.Author,
+                    Genre = b.Genre
+                }).SingleOrDefaultAsync(b => b.Id == id);
+
+            if(result == null)
+            {
+                return NotFound("That book isn't in our library");
+            } else
+            {
+                return Ok(result);
+            }
+        }
+
+
         [HttpGet("/books")]
         public async Task<IActionResult> GetAllBooks([FromQuery] string genre ="all")
         {
